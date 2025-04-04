c59982d5b3e1ba1de239e6361399d5b453852c98
Tatyana25081974 <tatyanakorabelnikovamama@gmail.com>
2025-04-04 19:02:23 +0300

    add initial files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ContactForm } from '../ContactForm/ContactForm';
+import { SearchBox } from '../SearchBox/SearchBox';
+import { ContactList } from '../ContactList/ContactList';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from '../../redux/contactsOps';
 
-function App() {
-  const [count, setCount] = useState(0)
+import css from './App.module.css';
 
+export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]); 
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className={css.container}>
+      <h1 className={css.title}>Phonebook</h1>
 
-export default App
+      <ContactForm />
+
+      <h2 className={css.subtitle}>Find contacts by name</h2>
+
+      <SearchBox />
+
+      <ContactList />
+    </div>
+  );
+}
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createSelector } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './contactsOps';
+import { selectNameFilter } from './filtersSlice';
 
 const contactsSlice = createSlice({
   name: 'contacts',
@@ -7,7 +9,61 @@ const contactsSlice = createSlice({
     loading: false,
     error: null,
   },
-  reducers: {}, 
-});
+  reducers: {},
+  extraReducers: builder => {
+    builder
+    // 🔹 Отримання контактів
+      .addCase(fetchContacts.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = action.payload; // отримали масив контактів
+      })
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload; // текст помилки
+      })
+     // 🔹 Додавання контакту
+      .addCase(addContact.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items.push(action.payload); // додаємо новий контакт
+      })
+      .addCase(addContact.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
 
+      // 🔹 Видалення контакту
+      .addCase(deleteContact.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = state.items.filter(contact => contact.id !== action.payload.id);
+      })
+      .addCase(deleteContact.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  }
+});
+export const selectContacts = state => state.contacts.items;
+export const selectLoading = state => state.contacts.loading;
+export const selectError = state => state.contacts.error;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectNameFilter],  // беремо всі контакти і поточний фільтр
+  (contacts, filter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
 export default contactsSlice.reducer;
